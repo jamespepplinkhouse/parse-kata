7a34a5a23f1c5216e031d9f673b2540b2861f3e8
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-03-23 22:07:22 +1000

    split + index

--- solutions/go-chunks-single-thread/parse.go
+++ solutions/go-chunks-single-thread/parse.go
@@ -8,29 +8,27 @@ import (
 
 func ParseLineJson(line []byte) (title []byte) {
 	jsonStartIndex := bytes.Index(line, []byte("{"))
-	return fastjson.GetBytes(line[jsonStartIndex:], "title")
-}
-
-func ParseLineRaw(line []byte) (title []byte) {
-	titleKeyStartIndex := bytes.Index(line, []byte("\"title\": \""))
-
-	if titleKeyStartIndex == -1 {
+	if jsonStartIndex == -1 {
 		// There was no title field in the line
 		return nil
 	}
 
-	// The title value starts at +10 from the leading quote of the json title key
-	titleValueStartIndex := titleKeyStartIndex + 10
+	return fastjson.GetBytes(line[jsonStartIndex:], "title")
+}
 
-	if titleValueStartIndex > len(line) {
+func ParseLineRaw(line []byte) (title []byte) {
+	titleStart := bytes.Split(line, []byte("\"title\": \""))
+
+	if len(titleStart) <= 1 {
 		return nil
 	}
 
-	titleLength := bytes.Index(line[titleValueStartIndex:], []byte("\""))
+	titleLength := bytes.Index(titleStart[1], []byte("\""))
 
 	if titleLength == -1 {
 		return nil
 	}
 
-	return line[titleValueStartIndex : titleValueStartIndex+titleLength]
+	return titleStart[1][:titleLength]
+
 }
