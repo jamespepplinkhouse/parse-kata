32d0a16e725a9dbaea5051821fa58db2cd1bc82e
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-05-27 09:13:11 +1000

    WIP

--- solutions/dotnet/Program.cs
+++ solutions/dotnet/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
+﻿using System.IO;
+
+var inputFile = args[0];
+// var outputFile = args[1];
+
+// var counter = 0;
+// foreach (var line in File.ReadLines(Path.GetFullPath(inputFile)))
+// {
+//   counter++;
+//   // Console.WriteLine(line);
+// }
+// Console.WriteLine($"Found {counter} lines");
+
+
+const int MAX_BUFFER = 1048576; //1MB 
+byte[] buffer = new byte[MAX_BUFFER];
+int bytesRead;
+var chunks = 0;
+using (var fs = File.Open(inputFile, FileMode.Open, FileAccess.Read))
+using (var bs = new BufferedStream(fs))
+{
+  while ((bytesRead = bs.Read(buffer, 0, MAX_BUFFER)) != 0) //reading 1mb chunks at a time
+  {
+    chunks++;
+    //Let's create a small size file using the data. Or Pass this data for any further processing.
+  }
+}
+
+var fn = () =>
+{
+  Console.WriteLine("Hai");
+};
+
+fn();
+
+Console.WriteLine($"Found {chunks} chunks");
+
+// Input file setup
+// Output file setup
+// parse
--- solutions/go-lines-single-thread/main.go
+++ solutions/go-lines-single-thread/main.go
@@ -12,17 +12,15 @@ import (
 func main() {
 	defer profile.Start(profile.ProfilePath(".")).Stop()
 
+	// TODO: be more specific, or use a library
 	args := os.Args[1:]
 	if len(args) != 2 {
 		log.Fatal("Incorrect arguments supplied")
 	}
-	// TODO: be more specific, or use a library
 
 	inputFile := args[0]
-	outputFile := args[1]
 
 	fmt.Println("Reading from: ", inputFile)
-	fmt.Println("Writing to:", outputFile)
 
 	// Input file setup
 	input, err := os.Open(inputFile)
@@ -31,28 +29,20 @@ func main() {
 	}
 	defer input.Close()
 
-	// Output file setup
-	output, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("Failed creating output file: %s", err)
-	}
-	datawriter := bufio.NewWriter(output)
-	defer datawriter.Flush()
-	defer output.Close()
-
 	scanner := bufio.NewScanner(input)
 	maxCapacity := 1024 * 1024
 	buf := make([]byte, maxCapacity)
 	scanner.Buffer(buf, maxCapacity)
 
-	newLine := []byte("\n")
+	counter := 0
 
 	for scanner.Scan() {
-		_, _ = datawriter.Write(ParseLineRaw(scanner.Bytes()))
-		datawriter.Write(newLine)
+		counter++
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println(counter)
 }
