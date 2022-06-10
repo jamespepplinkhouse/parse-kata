a35825dd6e6d2b44d6eae3564a2c50b602d358c8
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-06-10 22:09:50 +1000

    WIP

--- solutions/dotnet/Program.cs
+++ solutions/dotnet/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var inputFilePath = args[0];
+    var outputFilePath = args[1];
+    Console.WriteLine($"inputFilePath: {inputFilePath}");
+    Console.WriteLine($"outputFilePath: {outputFilePath}");
+
+    var title = new Regex("title\": \"(.+?)\"");
+
+    foreach (var line in File.ReadLines(inputFilePath))
+    {
+      var matches = title.Match(line);
+      if (matches.Groups.Count > 1)
+      {
+        Console.WriteLine(matches.Groups[1].Captures.First());
+      }
+    }
+  }
+}
