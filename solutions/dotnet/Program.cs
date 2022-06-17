c49fbcb43cd11ee455a1347092fba508b946ddca
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-06-17 13:31:10 +1000

    WIP: input stream output stream regex
    Compiled regex ~37s
    Without compiled regex ~62s

--- solutions/dotnet/Program.cs
+++ solutions/dotnet/Program.cs
@@ -1,26 +1,36 @@
 ﻿using System.Text.RegularExpressions;
+using System.Text;
 
 public class Program
 {
+  const int MAX_BUFFER = 1048576;
+
   public static void Main(string[] args)
   {
     var inputFilePath = args[0];
     var outputFilePath = args[1];
-    Console.WriteLine($"inputFilePath: {inputFilePath}");
-    Console.WriteLine($"outputFilePath: {outputFilePath}");
+    var titleRegex = new Regex("title\": \"(.+?)\"", RegexOptions.Compiled);
 
-    var title = new Regex("title\": \"(.+?)\"");
-
-    using (var outputFile = new StreamWriter(outputFilePath))
+    using (var outputFile = new FileStream(outputFilePath, FileMode.Create))
+    using (var fs = File.Open(inputFilePath, FileMode.Open, FileAccess.Read))
+    using (var bs = new BufferedStream(fs))
     {
-      foreach (var line in File.ReadLines(inputFilePath))
+      byte[] buffer = new byte[MAX_BUFFER];
+      int bytesRead;
+
+      while ((bytesRead = bs.Read(buffer, 0, MAX_BUFFER)) != 0)
       {
-        var matches = title.Match(line);
-        if (matches.Groups.Count > 1)
-        {
-          outputFile.WriteLine(matches.Groups[1].Captures.First());
-        }
+        // TODO: buffer might contain a chopped title at the end
+        // TODO: Don't convert to string, scan each byte once only
+        // TODO: Don't use regex, too slow!
+        // TODO: After a newline, skip 50 bytes
+        var content = Encoding.Default.GetString(buffer);
+        var matches = titleRegex.Matches(content);
+        var titles = matches.Select(x => x.Groups[1].Captures.First().Value);
+        outputFile.Write(Encoding.ASCII.GetBytes(String.Join('\n', titles)));
       }
     }
   }
 }
+
+
