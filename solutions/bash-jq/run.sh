692e7202c68da2e094fcbd7c8c6b9b2024dd22ea
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-07-31 14:40:57 +1000

    ...

--- solutions/bash-jq/run.sh
+++ solutions/bash-jq/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-cd "$(dirname "$0")"
 
 if [ -z ${1+x} ]; then echo "First parameter (input file path) not supplied"; exit 1; fi
 if [ -z ${2+x} ]; then echo "Second parameter (ouput file path) not supplied"; exit 1; fi
