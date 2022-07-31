b53bc8b540dbc0dfad729c9a35f95b830f47c533
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-07-31 14:26:12 +1000

    Correct outputs at last!

--- solutions/bash-jq/run.sh
+++ solutions/bash-jq/run.sh
@@ -5,4 +5,5 @@ cd "$(dirname "$0")"
 if [ -z ${1+x} ]; then echo "First parameter (input file path) not supplied"; exit 1; fi
 if [ -z ${2+x} ]; then echo "Second parameter (ouput file path) not supplied"; exit 1; fi
 
-awk -F'\t' '{print $5}' $1 | jq -r .title > $2
+# Note: for jq, --raw-output doesn't work with --ascii-output, hence I used awk to strip out the enclosing quotes
+awk -F'\t' '{print $5}' $1 | jq --ascii-output .title | awk '{print substr($0, 2, length($0) - 2)}' > $2
