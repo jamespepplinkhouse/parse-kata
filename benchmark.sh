4aa3bbe83be8c40af4ba1efe50c926e964e24501
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-07-31 14:56:04 +1000

    warmup

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-hyperfine \
+hyperfine --warmup 3 \
     'solutions/bash-jq/run.sh samples/10mb-input.txt data/10mb-bash-jq.txt' \
     'solutions/bash-sed/run.sh samples/10mb-input.txt data/10mb-bash-sed.txt' \
     'solutions/dotnet/ParseKata/bin/Release/net7.0/ParseKata samples/10mb-input.txt data/10mb-dotnet.txt' \
