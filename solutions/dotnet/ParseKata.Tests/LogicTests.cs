4b6264675bf32e59f9f097e2c34262cb0db63ce6
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-08-28 13:57:02 +1000

    dotnet - Handle escaped quotes inside titles

--- solutions/dotnet/ParseKata.Tests/LogicTests.cs
+++ solutions/dotnet/ParseKata.Tests/LogicTests.cs
@@ -19,6 +19,12 @@ public class LogicTests
     var result = Logic.ExtractTitles(titleBytes);
     Assert.Equal("Noir comme un ange\nLe chat sacr\u00e9 de Birmanie\n", Encoding.UTF8.GetString(result));
   }
+
+    [Fact]
+  public void ExtractTitle_With_Escaped_Quotes()
+  {
+    var titleBytes = Encoding.UTF8.GetBytes("/type/work	/works/OL10077387W	2	2020-08-01T08:00:37.626801	{\"last_modified\": {\"type\": \"/type/datetime\", \"value\": \"2020-08-01T08:00:37.626801\"}, \"title\": \"Set of Yearbooks of the Index Islamicus, 1906-2003 Index Islamicus 2001 =\\\"yearbook 2000\\\"\", \"created\": {\"type\": \"/type/datetime\", \"value\": \"2009-12-11T02:10:32.891218\"}, \"subjects\": [\"Islam, bibliography\", \"Islamic civilization\"], \"latest_revision\": 2, \"key\": \"/works/OL10077387W\", \"authors\": [{\"type\": {\"key\": \"/type/author_role\"}, \"author\": {\"key\": \"/authors/OL4058775A\"}}], \"type\": {\"key\": \"/type/work\"}, \"revision\": 2}");
+    var result = Logic.ExtractTitles(titleBytes);
+    Assert.Equal("Set of Yearbooks of the Index Islamicus, 1906-2003 Index Islamicus 2001 =\\\"yearbook 2000\\\"\n", Encoding.UTF8.GetString(result));
+  }
 }
-
-
--- solutions/dotnet/ParseKata/Logic.cs
+++ solutions/dotnet/ParseKata/Logic.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 public static class Logic
 {
-  private static byte[] TitleBytes = Encoding.ASCII.GetBytes("\"title\": \"");
+  private static byte[] TitleBytesMarker = Encoding.ASCII.GetBytes("\"title\": \"");
   private static byte QuoteByte = Encoding.ASCII.GetBytes("\"")[0];
+  private static byte EscapeByte = Encoding.ASCII.GetBytes("\\")[0];
   private static byte NewLineByte = Encoding.ASCII.GetBytes("\n")[0];
   private const int MAX_BUFFER = 1048576;
 
@@ -19,11 +20,11 @@ public static class Logic
 
     // Write any title data we find into this result buffer
     var titles = new byte[MAX_BUFFER];
-    
+
     // Maintain a cursor, which is the index of how much we wrote to the titles buffer
     int titlesCursor = 0;
 
-    int titleBytesLength = TitleBytes.Length;
+    int titleBytesLength = TitleBytesMarker.Length;
 
     while (chunkCursor < chunkLength)
     {
@@ -31,7 +32,7 @@ public static class Logic
       var searchSpan = chunkSpan.Slice(chunkCursor, chunkLength - chunkCursor);
 
       // Search for the next set of title bytes
-      var nextTitleFieldIndex = searchSpan.IndexOf(TitleBytes);
+      var nextTitleFieldIndex = searchSpan.IndexOf(TitleBytesMarker);
 
       // If we can't find a title, break 
       if (nextTitleFieldIndex == -1) break;
@@ -42,17 +43,38 @@ public static class Logic
       // Create a span which starts at the start of the title we found
       var titleSpan = chunkSpan.Slice(chunkCursor, chunkLength - chunkCursor);
 
-      // Find the end of the title
-      var titleEndIndex = titleSpan.IndexOf(QuoteByte);
+      var foundTitleEndIndex = false;
+      var maybeTitleEndIndex = titleSpan.IndexOf(QuoteByte);
 
-      // TODO: We need to check if the character before titleEndIndex is a backslash
-      // in that case it's an escaped quote, and we need to keep looking!
+      // Try to find the quote byte at the end of the title
+      // skipping over escaped quotation marks
+      while (!foundTitleEndIndex)
+      {
+        // If we can't find another quotation mark character, break
+        if (maybeTitleEndIndex == -1) break;
+
+        // If the character before the quotation mark is a escape byte (backslash)
+        if (maybeTitleEndIndex > 0 && titleSpan[maybeTitleEndIndex - 1] == EscapeByte)
+        {
+          // Advance the cursor past the escaped quotation we already found
+          var adjustedIndex = maybeTitleEndIndex + 1;
+
+          // Search again
+          var nextQuoteSearch = titleSpan.Slice(adjustedIndex, titleSpan.Length - adjustedIndex);
+          var nextQuoteSearchIndex = nextQuoteSearch.IndexOf(QuoteByte);
+
+          // Adjust the original search index 
+          maybeTitleEndIndex = nextQuoteSearchIndex > -1 ? adjustedIndex + nextQuoteSearchIndex : -1;
+        }
+        else
+          foundTitleEndIndex = true;
+      }
 
       // TODO: Handle the case where the end if the chunk is in the middle of a title
-      if (titleEndIndex == -1) break;
+      if (maybeTitleEndIndex == -1) break;
 
       // Create a span which has only the title in it
-      var title = titleSpan.Slice(0, titleEndIndex);
+      var title = titleSpan.Slice(0, maybeTitleEndIndex);
 
       // Loop over the title and set it's characters into the result buffer
       // incrementing the titles cursor
@@ -70,7 +92,7 @@ public static class Logic
       // Update the chunk cursor past the end of the title we processed, the extra
       // 50 chars here are because we know for sure (based on analysing the input file)
       // that the next title is more than 50 chars away
-      chunkCursor = chunkCursor + titleEndIndex + 50;
+      chunkCursor = chunkCursor + maybeTitleEndIndex + 50;
     }
 
     // Return the data from the start, up to where we filled it to
