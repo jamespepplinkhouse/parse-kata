ded800f9845b06f70f6abfa4235edbe27ae1b182
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2022-10-14 11:52:44 +1000

    Remove bash-sed implementation, it's too far from being correct

--- benchmark.sh
+++ benchmark.sh
@@ -3,7 +3,6 @@ set -e
 
 hyperfine --warmup 3 \
     -n bash-jq 'solutions/bash-jq/run.sh samples/10mb-input.txt data/10mb-bash-jq.txt' \
-    -n bash-sed 'solutions/bash-sed/run.sh samples/10mb-input.txt data/10mb-bash-sed.txt' \
     -n dotnet-json 'solutions/dotnet/ParseKata/bin/Release/net7.0/ParseKata -i samples/10mb-input.txt -o data/10mb-dotnet.txt' \
     -n dotnet-custom 'solutions/dotnet/ParseKata/bin/Release/net7.0/ParseKata -i samples/10mb-input.txt -o data/10mb-dotnet.txt -f' \
     -n golang 'solutions/go/parse-kata samples/10mb-input.txt data/10mb-go.txt' \
--- validate.sh
+++ validate.sh
@@ -8,12 +8,6 @@ solutions/bash-jq/run.sh samples/10mb-input.txt data/10mb-bash-jq.txt
 diff samples/10mb-output.txt data/10mb-bash-jq.txt | diffstat
 
 echo 
-echo -e "\033[0;33mbash-sed\033[0m"
-echo
-diff samples/10mb-output.txt data/10mb-bash-sed.txt | diffstat
-solutions/bash-sed/run.sh samples/10mb-input.txt data/10mb-bash-sed.txt
-
-echo 
 echo -e "\033[0;33mdotnet json\033[0m"
 echo
 solutions/dotnet/ParseKata/bin/Release/net7.0/ParseKata -i samples/10mb-input.txt -o data/10mb-dotnet-json.txt
