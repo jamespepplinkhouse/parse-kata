93bedb21b6e084f09fc637c3c75036941b9395e8
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2023-05-27 14:32:05 +1000

    Make main nicer

--- solutions/rust/src/main.rs
+++ solutions/rust/src/main.rs
@@ -3,7 +3,6 @@ use parse_kata::process_input_file_json;
 use std::error::Error;
 use structopt::StructOpt;
 
-/// A basic example
 #[derive(StructOpt, Debug)]
 #[structopt(
     name = "parse-kata",
@@ -27,17 +26,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
     println!("{:?}", opt);
 
-    if opt.fast_mode {
-        process_input_file_bytes(&opt.input_file, &opt.output_file)
-            .map_err(|err| println!("Error processing file: {}", err))
-            .ok();
-    } else {
-        process_input_file_json(&opt.input_file, &opt.output_file)
-            .map_err(|err| println!("Error processing file: {}", err))
-            .ok();
-    }
+    let process = match opt.fast_mode {
+        true => {
+            println!("Fast mode enabled: using custom byte parser");
+            process_input_file_bytes
+        }
+        false => {
+            println!("Normal mode enabled: using stream of lines and JSON parser");
+            process_input_file_json
+        }
+    };
 
-    println!("Processing complete.");
+    process(&opt.input_file, &opt.output_file)
+        .map_err(|err| println!("Error processing file: {}", err))
+        .ok();
+
+    println!("Processing complete!");
 
     Ok(())
 }
