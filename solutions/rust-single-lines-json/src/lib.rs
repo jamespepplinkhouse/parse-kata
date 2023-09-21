2e27054711d33a735aaeaf247348196391ec3521
James Pepplinkhouse <james.pepplinkhouse@gmail.com>
2023-09-21 14:26:45 +1000

    Revert "Rust producer-consumer model, 1.48x slower than single thread!"
    This reverts commit e7dea79505f48049b1bde097d67efc1807109227.

--- solutions/rust-single-lines-json/src/lib.rs
+++ solutions/rust-single-lines-json/src/lib.rs
@@ -3,53 +3,49 @@ use simd_json::ValueAccess;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::sync::mpsc::channel;
-use std::thread;
 
 pub fn process_input_file_json(
     input_file_path: &str,
     output_file_path: &str,
 ) -> Result<(), Box<dyn Error>> {
-    let (tx, rx) = channel::<Vec<u8>>();
-    let output_path_clone = output_file_path.to_owned(); // Clone for the worker thread
-
-    // Worker thread for JSON parsing and title extraction
-    let handle = thread::spawn(move || -> Result<(), Box<std::io::Error>> {
-        let output_file = File::create(output_path_clone)?;
-        let mut output_buffered_writer = BufWriter::new(output_file);
-        for mut json_data in rx.iter() {
-            let parsed: Result<Value, _> = simd_json::from_slice(&mut json_data);
-            if let Ok(json_value) = parsed {
-                if let Some(title_str) = json_value.get("title").and_then(|t| t.as_str()) {
-                    output_buffered_writer.write(format!("{}\n", title_str).as_bytes())?;
-                }
-            }
-        }
-        // Flush the writer
-        output_buffered_writer.flush()?;
-        Ok(())
-    });
-
-    // Main thread for reading input file
+    // Input file
     let input_file = File::open(input_file_path)?;
     let mut reader = BufReader::new(input_file);
+
+    // Output file
+    let output_file = File::create(output_file_path)?;
+    let mut output_buffered_writer = BufWriter::new(output_file);
+
+    // Buffer for each line
     let mut line_buffer = Vec::new();
 
     loop {
         line_buffer.clear();
 
+        // Read line into buffer
         let bytes_read = reader.read_until(b'\n', &mut line_buffer)?;
+
+        // Exit loop if EOF
         if bytes_read == 0 {
             break;
         }
 
-        if let Some(json_start_index) = line_buffer.iter().position(|&x| x == b'{') {
-            tx.send(line_buffer[json_start_index..].to_vec()).unwrap();
+        // Find the start of the JSON document
+        let json_start_index = line_buffer.iter().position(|&x| x == b'{');
+
+        let json_value: Result<Value, _>;
+        if let Some(json_start_index) = json_start_index {
+            json_value = simd_json::from_slice(line_buffer[json_start_index..].as_mut());
+
+            if let Some(title_str) = json_value.get("title").and_then(|t| t.as_str()) {
+                output_buffered_writer.write(format!("{}\n", title_str).as_bytes())?;
+            }
         }
     }
 
-    drop(tx); // Close the channel
-    handle.join().unwrap()?; // Wait for worker thread to finish
+    // Flush the writer
+    output_buffered_writer.flush()?;
+
     Ok(())
 }
 
